e89bfdfe8ccf70b2027742f4386809435110229c
Martin <dobosmartin595@gmail.com>
2023-03-06 22:00:38 +0100

    Importing maven project to make it work

--- spring/src/main/java/sk/martindobos/spring/repositories/MovieRepository.java
+++ spring/src/main/java/sk/martindobos/spring/repositories/MovieRepository.java
@@ -1,10 +1,10 @@
 package sk.martindobos.spring.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import sk.martindobos.spring.models.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
 
 
 }
--- spring/src/main/java/sk/martindobos/spring/services/implementation/EnglishImpl.java
+++ spring/src/main/java/sk/martindobos/spring/services/implementation/EnglishImpl.java
@@ -1,5 +1,6 @@
 package sk.martindobos.spring.services.implementation;
 
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import sk.martindobos.spring.services.ExampleService;
